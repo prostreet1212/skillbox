4c771b222bb7e83f3cd5e0a01ee5555faaedb737
prostreet1212 <prostreet1212@gmail.com>
2023-06-07 09:14:10 +0300

    dz3

--- module_21/homework_3/lib/repository/secure_storage/card_storage.dart
+++ module_21/homework_3/lib/repository/secure_storage/card_storage.dart
@@ -1,18 +1,13 @@
-
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+class CardStorage {
+  late Stream<Map<String, String>> cardStream;
 
-class CardStorage{
-
-  late Stream<Map<String,String>>  cardStream;
-
-  FlutterSecureStorage storage=const FlutterSecureStorage(
+  FlutterSecureStorage storage = const FlutterSecureStorage(
       aOptions: AndroidOptions(encryptedSharedPreferences: true),
-      iOptions: IOSOptions(accessibility:KeychainAccessibility.first_unlock)
-  );
+      iOptions: IOSOptions(accessibility: KeychainAccessibility.first_unlock));
 
- /* Future<Map<String,String>> getAllCards()async{
+  /* Future<Map<String,String>> getAllCards()async{
     Map<String,String> a={};
     await storage.readAll().then((value) {
       a=value;
@@ -20,24 +15,21 @@ class CardStorage{
     return a;
   }*/
 
-  Future<Map<String,String>> readAllSecureData() async {
+  Future<Map<String, String>> readAllSecureData() async {
     var allData = await storage.readAll();
     return allData;
   }
 
-  Stream<Map<String,String>> asdf() async* {
+  Stream<Map<String, String>> asdf() async* {
+    final allValues = await storage.readAll();
+    yield allValues;
+  }
 
-      final allValues = await storage.readAll();
-      yield allValues;
-    }
+  FlutterSecureStorage getStorage() {
+    return storage;
+  }
 
-  FlutterSecureStorage getStorage(){
-  return storage;
-}
-
-  Future<void>addCard(String key,String cardNumber)async{
-   await storage.write(key: key, value: cardNumber);
-
-}
-
+  Future<void> addCard(String key, String cardNumber) async {
+    await storage.write(key: key, value: cardNumber);
+  }
 }
--- module_21/homework_3/lib/screens/db_screen.dart
+++ module_21/homework_3/lib/screens/db_screen.dart
@@ -12,69 +12,79 @@ class DbScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context)=>DbBloc(UserDatabase(),CardStorage()/*UserDatabase()*/)..add(GetAllUsersEvent()),
+      create: (context) =>
+          DbBloc(UserDatabase(), CardStorage() /*UserDatabase()*/)
+            ..add(GetAllUsersEvent()),
       child: Scaffold(
-      appBar: AppBar(),
-      body:  BlocBuilder<DbBloc, DbState>(
-          builder: (context, state) {
-            if (state is InitDb) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else {
-              if ((state as AllUsersState).users.isNotEmpty) {
-                var users = state.users;
-                Map<String,String> cards=state.cardNumbers;
-                print('length^ ${cards.length}');
+          appBar: AppBar(),
+          body: BlocBuilder<DbBloc, DbState>(
+            builder: (context, state) {
+              if (state is InitDb) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                if ((state as AllUsersState).users.isNotEmpty) {
+                  var users = state.users;
+                  Map<String, String> cards = state.cardNumbers;
+                  print('length^ ${cards.length}');
 
-                return ListView.builder(
-                    itemCount: users.length,
-                    itemBuilder: (context, i) {
-                      return GestureDetector(
-                        onLongPress: () {
-                          /* BlocProvider.of<DbBloc>(context).add(UpdateUserEvent(
+                  return ListView.builder(
+                      itemCount: users.length,
+                      itemBuilder: (context, i) {
+                        return GestureDetector(
+                          onLongPress: () {
+                            /* BlocProvider.of<DbBloc>(context).add(UpdateUserEvent(
                             userHelper: UserHelper(name: '123', surname: '456'),
                             id: users[i].id));*/
-                        },
-                        child: Dismissible(
-                          key: Key(const Uuid().v1()),
-                          onDismissed: (direction) {
-                            BlocProvider.of<DbBloc>(context)
-                                .add(DeleteUserEvent(index: users[i].id));
                           },
-                          child:cards['${users[i].id.toString()}_${users[i].name}']!=null?Card(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(users[i].id.toString()),
-                                Text(users[i].name),
-                                Text(users[i].surname),
-                                Text(cards['${users[i].id.toString()}_${users[i].name}']??'error'),
-                              ],
-                            ),
-                          ):Center(child: CircularProgressIndicator(),),
-                        ),
-                      );
-                    });
-              } else {
-                return const Text('empty');
+                          child: Dismissible(
+                            key: Key(const Uuid().v1()),
+                            onDismissed: (direction) {
+                              BlocProvider.of<DbBloc>(context)
+                                  .add(DeleteUserEvent(index: users[i].id));
+                            },
+                            child:
+                                cards['${users[i].id.toString()}_${users[i].name}'] !=
+                                        null
+                                    ? Card(
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Text(users[i].id.toString()),
+                                            Text(users[i].name),
+                                            Text(users[i].surname),
+                                            Text(cards[
+                                                    '${users[i].id.toString()}_${users[i].name}'] ??
+                                                'error'),
+                                          ],
+                                        ),
+                                      )
+                                    : const Center(
+                                        child: CircularProgressIndicator(),
+                                      ),
+                          ),
+                        );
+                      });
+                } else {
+                  return const Text('empty');
+                }
               }
-            }
-          },
-        ),
-      floatingActionButton:Builder(
-        builder: (context){
-          return FloatingActionButton(
-            mini: true,
-            child: const Icon(Icons.add),
-            onPressed: () async{
-              BlocProvider.of<DbBloc>(context).add(InsertUserEvent(
-                  userHelper: UserHelper(name: 'test', surname: 'testov',cardNumber: 2202)));
-
             },
-          );
-        },
-      )
-    ),);
+          ),
+          floatingActionButton: Builder(
+            builder: (context) {
+              return FloatingActionButton(
+                mini: true,
+                child: const Icon(Icons.add),
+                onPressed: () async {
+                  BlocProvider.of<DbBloc>(context).add(InsertUserEvent(
+                      userHelper: UserHelper(
+                          name: 'test', surname: 'testov', cardNumber: 2202)));
+                },
+              );
+            },
+          )),
+    );
   }
 }
